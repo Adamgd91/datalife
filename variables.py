2548a39714c4d09464ecb101ad88810f4d78e513
Adam Davidson <agdavidson91@gmail.com>
2025-09-17 08:12:40 -0400

    ddsa

--- variables.py
+++ variables.py
@@ -18,3 +18,23 @@ print(bmi)
 
 print(type(accomplished))
 print(type(bmi))
+
+
+#################### LIST #####################
+davidson_family = ["Adam", "Sarah", "Harper"]
+print(davidson_family)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# House information as list of lists
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+        ["bedroom", bed],
+        ["bathroom", bath]]
+
+# Print out house
+print(house)
