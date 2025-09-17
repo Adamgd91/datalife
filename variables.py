42ffddecd0f1c6fd7aa24fa92ec5b16584e9a3fc
Adam Davidson <agdavidson91@gmail.com>
2025-09-17 06:41:31 -0400

    dddsa

--- variables.py
+++ variables.py
@@ -1,23 +1,5 @@
 fname = "Adam"
-lname = "davidson"
-age = 34
-data_driven = False
-family = ["Adam", "Sarah", "Harper"]
-books = {
-    "LOTR": 443,
-    "Harry": 543,
-    "FWings": 322
-}
+lname = "Davidson"
 
-print(fname)
-print(f'My name is {fname} {lname} and I am {age} years old!')
-
-
-age = 22
-print(age)
-
-Fnames = ["adam", "jongnogivhw;ov", 22, 34]
-
-for fname in Fnames:
-    print(fname)
-    
+full_name = f'My name is {fname} {lname}'
+print(full_name)
