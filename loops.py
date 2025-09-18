7539e199832badefd6225912760979c9e296f525
Adam Davidson <agdavidson91@gmail.com>
2025-09-18 08:23:15 -0400

    added failed scores to looping

--- loops.py
+++ loops.py
@@ -1,5 +1,15 @@
 areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
-
 for i in areas:
     if i == "bedroom":
         print(areas[5])
+        
+        
+
+scores = [12,9,8,34,22,1,14]
+failed = []
+for i in scores:
+    if i < 12:
+        failed.append(i)
+
+num_failed = len(failed)
+print(num_failed)
