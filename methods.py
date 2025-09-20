f4a4cc45d86236f1886b4dba1dcadad8484219c2
Adam Davidson <agdavidson91@gmail.com>
2025-09-20 06:52:38 -0400

    addeed methods

---
+++ methods.py
@@ -0,0 +1,10 @@
+name = "adam"
+fam_names = ["Bill", "Diane", "Bryan", "Matt", "Adam"]
+
+cap_name = name.capitalize()
+print(cap_name)
+index_fam = fam_names.index("Bryan")
+print(index_fam)
+upper_name = name.upper()
+print(upper_name)
+print(name.count("a"))
