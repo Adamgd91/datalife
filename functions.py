abcde2be038bb8244687cd18682d2f60cc2401bb
Adam Davidson <agdavidson91@gmail.com>
2025-09-20 06:28:05 -0400

    adding drinking function

--- functions.py
+++ functions.py
@@ -9,5 +9,22 @@ testing()
 
 ######## BUILT IN FUNCTIONS ##########
 ages = [11, 32, 54, 65, 22, 31 ,76, 66, 54,]
+can_drink = []
+too_young = []
 oldest = max(ages)
 print(oldest)
+
+def drinking_age():
+    for i in ages:
+        if i >= 21:
+            can_drink.append(i)
+        else:
+            too_young.append(i)
+drinking_age()
+num_drinking = len(can_drink)
+num_not_drinking = len(too_young)
+print(can_drink)
+print(num_drinking)
+
+print(too_young)
+print(num_not_drinking)
