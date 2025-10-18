c9ee2700e1b43da56a555a6f43d0e644b7642b95
Adam Davidson <agdavidson91@gmail.com>
2025-10-18 09:30:46 -0400

    i'm back

---
+++ vfldflie.py
@@ -0,0 +1,10 @@
+va1 = "Adam"
+va2 = "Davidson"
+age = 34
+profession = "AI/ML Engineer"
+is_meet = False
+family_names = ["Adam", "Sarah", "Harper"]
+ages = [34, 32, 1]
+
+full_name = f'{va1} {va2}'
+print(full_name)
