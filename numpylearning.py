80e3d238b255f5b08dad9b27288793d60df95e9e
Adam Davidson <agdavidson91@gmail.com>
2025-10-19 08:45:24 -0400

    asddd

--- numpylearning.py
+++ numpylearning.py
@@ -24,3 +24,10 @@ np_baseball = np.array(baseball)
 print(np_baseball)
 print(np_baseball.shape)
 print(np_baseball[2,1])
+
+# Create np_height_in from np_baseball
+np_height_in = np_baseball[:,0]
+# Print out the mean of np_height_in
+print(np.mean(np_height_in))
+# Print out the median of np_height_in
+print(np.median(np_height_in))
