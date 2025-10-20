c4252fe88b2c4036292ad0f08a91ed8bd06052b2
Adam Davidson <agdavidson91@gmail.com>
2025-10-20 06:59:40 -0400

    prac

--- vfldflie.py
+++ vfldflie.py
@@ -1,3 +1,5 @@
+########## VARIABLES & F-STRING ###########
+
 va1 = "Adam"
 va2 = "Davidson"
 age = 34
@@ -8,3 +10,20 @@ ages = [34, 32, 1]
 
 full_name = f'{va1} {va2}'
 print(full_name)
+
+########## DICTS & LISTS ###########
+
+scores = [55, 66, 76, 99, 87, 76, 68, 98, 88, 87, 80, 78, 75, 69, 100]
+passed_scores = []
+failed_scores = []
+
+for i in scores:
+    if i >= 70:
+        passed_scores.append(i)
+    else:
+        failed_scores.append(i)
+        
+count_passed = len(passed_scores)
+count_failed = len(failed_scores)   
+print(f'There are {count_passed} scores that passed : {passed_scores}')
+print(f'There are {count_failed} scores that passed : {failed_scores}')
